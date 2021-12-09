d9dfd8a2f69830022dd64735e639ba0f547bd687
Henrik Steen <henrist@henrist.net>
2021-12-09 14:10:28 +0100

    chore(deps): update linters and semantic release

--- src/handlers/util/logger.ts
+++ src/handlers/util/logger.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
